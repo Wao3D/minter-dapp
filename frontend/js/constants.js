28d99ceef3d7edb6cd8a136b87b7674f43d507ea
Wao3d <wao3dstudio@gmail.com>
2022-07-17 14:31:08 +0200

    Rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,10 +1,10 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x0C6B019FB738eEc224601988C30b0f48d7A12248"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const welcome_h1 = "Welcome to the Cooupers NFT Project!!";
+const welcome_h2 = "Connect to MetaMask to Get Started"; 
+const welcome_p = 'The Cooupers NFT Project is a decentralized, open-source project.';
+const h1_presale_coming_soon = "NFT Mint Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
