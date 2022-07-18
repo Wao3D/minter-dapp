5519043906eeb4f44ae39fe1604fdbb7adaf7093
Wao3d <wao3dstudio@gmail.com>
2022-07-18 20:51:55 +0200

    logo

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,10 +58,10 @@ const MINT_PRICE = 0.000001; // Minting price per NFT. Rinkeby = ETH, Ethereum =
 const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-08-06T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = null;//"2022-08-06T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-08-05T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = null;//"2022-08-05T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x7BC92CCba46c1C5A1C365F5621FA29736ae0E5be"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
@@ -74,7 +74,7 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = null; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "Come join the wonderful world of Cooupers, a collection of 10.000 octopus with plenty of variations, get yours until it's swims away."; // Replace with what you want the generic descriptions to say.
+const GENERIC_DESCRIPTION = "Come join the wonderful world of Cooupers, a NFT collection of 10.000 octopus with plenty of variations and styles, get yours until it's swims away."; // Replace with what you want the generic descriptions to say.
 const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreiegnfuf2ji3qcgj4vmlio7oghwvwx6mwlal4rd75rjtyse4boowty"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
